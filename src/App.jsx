78836e942d579f54361d1d82f94cfc9bda1a9800
Neung0511 <neung@gmail.com>
2023-12-21 13:07:19 +0700

    states:completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const [InputGreetingMessage, setInputGreeting] = useState("");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+
+        <input
+          id="greeting-message"
+          type="text"
+          value={InputGreetingMessage}
+          onChange={(event) => setInputGreeting(event.target.value)}
+        />
+
+        <button
+          onClick={() => {
+            setGreetingMessage(InputGreetingMessage);
+          }}>
+          Update Text
+        </button>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setGreetingMessage("สวัสดี");
+          }}>
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setGreetingMessage("Hi!");
+          }}>
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setGreetingMessage("你好!");
+          }}>
+          你好!
+        </button>
       </div>
     </div>
   );
